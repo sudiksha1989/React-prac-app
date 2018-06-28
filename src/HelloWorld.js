3e5929a661b6b9b7e94e06f6948df27f0cd7b40e
sudiksha1989 <snagvanshi06@gmail.com>
2018-06-28 16:43:50 +0530

    recent chnge

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,5 +1,29 @@
 import React,{Component} from 'react';
 import './HelloWorld.css'
+import d2 from 'd2/lib/d2';
+import HeaderBar from '@dhis2/d2-ui-header-bar';
+// Using CommonJS imports
+//var d2 = require('d2/lib/d2');
+import { init } from 'd2/lib/d2';
+import { getInstance } from 'd2/lib/d2';
+
+
+init({ baseUrl: 'http://88.80.191.115:29014/mh_revamping/api/' })
+  .then(d2 => {
+    //Your d2 is initialised and ready to use.
+    console.log("Your d2 is initialised and ready to use.")
+  });
+ getInstance()
+      .then(d2 => {
+          console.log(d2.currentUser.name); // Will log when the init() function is done initialising the instance
+      });
+
+
+   
+
+      export default function(props) {
+          return (<HeaderBar d2={props.d2}/>)
+      }
 
 
 class HelloWorld extends Component{
@@ -7,16 +31,22 @@ class HelloWorld extends Component{
     super(props);
     this.state={greeting:'hello--'}
     //this.frenchify=this.frenchify.bind( this)
+    //this.removeGreeting =this.removeGreeting .bind(this);
   }
   frenchify(){
     this.setState({greeting:'Bonjour'})
+  }
+  removeGreeting(){
+    this.props.removeGreeting(this.props.name)
   }
   render(){
     return (
     <div className='HelloWorld'>
     {this.state.greeting} {this.props.name}!
     <br/>
-    <button onClick={this.frenchify.bind( this)}>frenchify!</button>
+    <button onClick={this.frenchify.bind( this)}>frenchify</button>
+    <br/><br/>
+    <button onClick={this.removeGreeting.bind( this)}>Remove me</button>
     </div>)
 
   }
--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -11,19 +11,29 @@ class HelloWorldList extends Component{
         super(props);
         this.state={greeting:['Jim','Sally','Bender']}
         this.addGreeting =this.addGreeting.bind(this);
+        this.removeGreeting=this.removeGreeting.bind(this)
     }
     addGreeting (newName){
         this.setState({greeting:[...this.state.greeting,newName]})
     }
+    removeGreeting(removeName){
+        const filteredGreeting=this.state.greeting.filter(name=>{
+            return name!==removeName
+        })
+        this.setState({greeting:filteredGreeting})
+    }
     renderGreetings(){
         return this.state.greeting.map(name=>(
-            <HelloWorld key={name} name={name}/>
+            <HelloWorld key={name} name={name}
+          removeGreeting={this.removeGreeting}
+          />
         ))
     }
+    
     render(){
         return (
             <div className='HelloWorldList'>
-                <AddGreeter AddGreeter={this.addGreeting}/>
+                <AddGreeter addGreeting={this.addGreeting}/>
                 {this.renderGreetings()}         
             </div>
         )

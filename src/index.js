f28bc3e3088ee186c9e8a5a9912a28b990c7004b
sudiksha1989 <snagvanshi06@gmail.com>
2018-07-03 16:55:27 +0530

    latest

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import d2 from 'd2/lib/d2';
 
 import { init } from 'd2/lib/d2';
-import { getInstance } from 'd2/lib/d2';
 
+import { getInstance } from 'd2/lib/d2';
 
 let baseUrl = (window.location.href).split("api");
 let newbaseUrl=baseUrl[0]+"/api";
 
-console.log("baseUrl----"+baseUrl[0]+"/api");
+console.log("baseUrl----"+newbaseUrl[0]+"/api");
 
 init({ baseUrl: newbaseUrl })
   .then(d2 => {
@@ -22,9 +22,35 @@ init({ baseUrl: newbaseUrl })
  getInstance()
       .then(d2 => {
           console.log(d2.currentUser.name); // Will log when the init() function is done initialising the instance
+         // console.log(Object.keys(d2.models));
+         d2.models.organisationUnit.get('b3vBdsycgAD').then(organisationUnitModel => console.log(organisationUnitModel.name));
+         
+         d2.models.organisationUnit.list().then( organisationUnitCollection => 
+             console.log(organisationUnitCollection));
+         d2.models.organisationUnit .filter().on('level').equals(2) .list() .then(
+             organisationUnitCollection => 
+             console.log(organisationUnitCollection));
+         d2.models.organisationUnit .filter().on('level').equals(2) .list()
+         .then(organisationUnitCollection => { 
+            let organisationUnits = organisationUnitCollection.toArray()
+            let organisationUnitsm = [...organisationUnitCollection.values()]
+            console.log("organisationUnits-----"+organisationUnitsm[0].name+" "+organisationUnitsm[0].code);
+         }); 
+             
+
+         const organisationUnitsOnLevel3 = d2.models.organisationUnit .filter().on('level').equals(3);
+         const organisationUnitsOnLevel3WithParent = organisationUnitsOnLevel3 .filter().on('parent.id').equals('b3vBdsycgAD');
+         organisationUnitsOnLevel3.list({ paging: false }).then(organisationUnitCollection => { 
+           let organisationUnitsm = [...organisationUnitCollection.values()]
+
+           organisationUnitsm.forEach(function(organisationUnitsm) {
+            console.log("organisationUnits-----"+organisationUnitsm.name+" "+organisationUnitsm.code);
+          });
+            
+         }); 
+        // console.log("organisationUnits-----"+organisationUnitsOnLevel3WithParent[0].name+" "+organisationUnitsOnLevel3WithParent[0].code); 
       });
-
-
-
+ //d2.models.organisationUnit.get('b3vBdsycgAD').then(organisationUnitModel => console.log(organisationUnitModel)); 
+ //d2.models.organisationUnit.list() .then(organisationUnitCollection => console.log(organisationUnitCollection));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
